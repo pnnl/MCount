193d7af534054682f9eedc376626711f2192f14c
SuperBrainiac47 <navaj.nune@pnnl.gov>
2023-08-15 12:07:22 -0700

    n

--- internal/scripts/createSavableCountingDirectory.py
+++ internal/scripts/createSavableCountingDirectory.py
@@ -5,6 +5,8 @@ import urllib.request
 from pathlib import Path
 
 def creatCountDirectorySaving (imageList, countName):
+    #the follwing will make all the folders to organize the fidderent parts of detection and counting and will skip making the forlder if it already exists 
+
     locationForThis = "external\\detections\\"+  countName
     try:
         os.mkdir(ntpath.abspath(locationForThis))
@@ -40,12 +42,13 @@ def creatCountDirectorySaving (imageList, countName):
         print("skiped")
 
 
-    
+    #this creates the main excell sheet
     location = "external\\detections\\"+  countName + "\\spreadsheets"
     musselSheet = pd.ExcelWriter(location + "\\" + 'overall_mussel_counts.xlsx', engine='xlsxwriter')
 
     dfFull = pd.DataFrame({'File Name': imageList, })
 
+    #adds the names of the pictuers in this count set
     dfFull.to_excel(musselSheet, sheet_name='Total', index=False)
 
     musselSheet.close()
