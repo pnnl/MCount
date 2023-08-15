e27a297d877bf02259138a80f1e69702752763c1
SuperBrainiac47 <navaj.nune@pnnl.gov>
2023-08-15 12:45:19 -0700

    n

--- internal/scripts/createSavableCountingDirectory.py
+++ internal/scripts/createSavableCountingDirectory.py
@@ -6,43 +6,44 @@ from pathlib import Path
 
 def creatCountDirectorySaving (imageList, countName):
     #the follwing will make all the folders to organize the fidderent parts of detection and counting and will skip making the forlder if it already exists 
+    cwd = os.getcwd()
 
-    locationForThis = "external\\detections\\"+  countName
+    locationForThis = cwd + "\\external\\detections\\"+  countName
     try:
         os.mkdir(ntpath.abspath(locationForThis))
     except:
-        print("skiped")
+        print("skipped")
 
     
-    locationForThis = "external\\detections\\"+  countName + "\\images"
+    locationForThis = cwd + "\\external\\detections\\"+  countName + "\\images"
     try:
         os.mkdir(ntpath.abspath(locationForThis))
     except:
-        print("skiped")
+        print("skipped")
 
     
-    locationForThis = "external\\detections\\"+  countName + "\\images\\segmentation"
+    locationForThis = cwd + "\\external\\detections\\"+  countName + "\\images\\segmentation"
     try:
         os.mkdir(ntpath.abspath(locationForThis))
     except:
-        print("skiped")
+        print("skipped")
 
     
-    locationForThis = "external\\detections\\"+  countName + "\\images\\thresholding"
+    locationForThis = cwd + "\\external\\detections\\"+  countName + "\\images\\thresholding"
     try:
         os.mkdir(ntpath.abspath(locationForThis))
     except:
-        print("skiped")
+        print("skipped")
 
     
-    locationForThis = "external\\detections\\"+  countName + "\\spreadsheets"
+    locationForThis = cwd + "\\external\\detections\\"+  countName + "\\spreadsheets"
     try:
         os.mkdir(ntpath.abspath(locationForThis))
     except:
-        print("skiped")
+        print("skipped")
 
 
-    #this creates the main excell sheet
+    #this creates the main excel sheet
     location = "external\\detections\\"+  countName + "\\spreadsheets"
     musselSheet = pd.ExcelWriter(location + "\\" + 'overall_mussel_counts.xlsx', engine='xlsxwriter')
 
